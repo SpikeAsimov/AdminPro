3d420ab60cb190aa9aeadc23b00a4f020e854c17
spikeasimov <hualterio@gmail.com>
2020-02-03 23:00:15 -0300

    Agrega componentes dashboard, grafico-uno, progress en carpeta Pages

---
+++ src/app/pages/grafica-uno/grafica-uno.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-grafica-uno',
+  templateUrl: './grafica-uno.component.html',
+  styles: []
+})
+export class GraficaUnoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
