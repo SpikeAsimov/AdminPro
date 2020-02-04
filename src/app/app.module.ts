6bef6cb753f4848590b8f140e172d1d5645410ed
spikeasimov <hualterio@gmail.com>
2020-02-03 23:02:20 -0300

    mueve pagenotfound a pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { PagenotfoundComponent } from './pages/pagenotfound/pagenotfound.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { ProgressComponent } from './pages/progress/progress.component';
 import { GraficaUnoComponent } from './pages/grafica-uno/grafica-uno.component';
