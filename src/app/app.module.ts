950ea8fdc587e5dde1c5f3dfbc0868c375246adc
spikeasimov <hualterio@gmail.com>
2020-02-08 20:20:17 -0300

    se importa app.routes en app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Rutas
+import {APP_ROUTES} from './app.routes';
+
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { PagenotfoundComponent } from './pages/pagenotfound/pagenotfound.component';
@@ -10,6 +13,7 @@ import { GraficaUnoComponent } from './pages/grafica-uno/grafica-uno.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
 import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+
 
 @NgModule({
   declarations: [
@@ -24,7 +28,8 @@ import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component
     BreadcrumbsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTES
   ],
   providers: [],
   bootstrap: [AppComponent]
